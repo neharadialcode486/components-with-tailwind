f7407d8274b4e5cd1531ab9be08a56e6d7ca66d0
akash-535 <akashbish9e@gmail.com>
2024-10-15 15:49:34 +0530

    tab1 complete

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,3 +1,4 @@
+// Tabs
 const tabs = document.querySelectorAll('.tab');
 const contents = document.querySelectorAll('.tabsInfo');
 const tabHeads = document.querySelectorAll('.tabHead'); 
@@ -22,5 +23,30 @@ tabs.forEach(tab => {
         const target = tab.getAttribute('data-target');
         const targetContent = document.getElementById(target);
         targetContent && targetContent.classList.remove('hidden');
+    });
+});
+
+// Accordion
+const accordionItems = document.querySelectorAll('.accordionItem');
+accordionItems.forEach(item => {
+    const header = item.querySelector('.accordionHeader');
+    const content = item.querySelector('.accordionInfo');
+    const icon = item.querySelector('img');
+    
+    header.addEventListener('click', () => {
+        const accordionActive = document.querySelector('.accordionItem.active');
+        
+        if (accordionActive && accordionActive !== item) {
+            accordionActive.classList.remove('active');
+            accordionActive.querySelector('.accordionInfo').classList.add('hidden'); 
+            accordionActive.querySelector('img').src = './assets/images/svg/plus.svg';                   
+        }
+        item.classList.toggle('active');
+        content.classList.toggle('hidden');
+        icon.classList.toggle('rotate-[180deg]');
+        icon.classList.toggle('duration-[1s]');
+        icon.src = item.classList.contains('active') 
+            ? './assets/images/svg/minus.svg' 
+            : './assets/images/svg/plus.svg';
     });
 });
