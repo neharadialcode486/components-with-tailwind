90754dc8504eda63a0c80d65e77f62705a9517ef
akash-535 <akashbish9e@gmail.com>
2024-10-16 09:09:36 +0530

    duration add on accordion info

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,5 +1,3 @@
-const accordionItems = document.querySelectorAll('.accordionItem');
-
 accordionItems.forEach(item => {
     const header = item.querySelector('.accordionHeader');
     const content = item.querySelector('.accordionInfo');
@@ -12,27 +10,30 @@ accordionItems.forEach(item => {
 
         if (accordionActive && accordionActive !== item) {
             accordionActive.classList.remove('active');
+            accordionActive.querySelector('.accordionInfo').style.maxHeight = null;
             accordionActive.querySelector('.accordionInfo').classList.add('hidden');
             accordionActive.querySelector('img').classList.remove('rotate-180');
             accordionActive.querySelectorAll('.accordionHead').forEach(heading => {
                 heading.classList.remove('font-semibold');
                 heading.classList.add('font-normal');
-            });            
+            });
             accordionActive.querySelector('.accordionLine').classList.add('hidden');
         }
 
         item.classList.toggle('active');
-        content.classList.toggle('hidden');
-        icon.classList.toggle('rotate-180');
-        
-        // Toggle font-weight classes
         if (item.classList.contains('active')) {
+            content.style.maxHeight = content.scrollHeight + 'px'; // Adjust height dynamically
+            content.classList.remove('hidden');
+            icon.classList.add('rotate-180');
             headings.forEach(heading => {
                 heading.classList.add('font-semibold');
                 heading.classList.remove('font-normal');
             });
             line.classList.remove('hidden');
         } else {
+            content.style.maxHeight = null;
+            content.classList.add('hidden');
+            icon.classList.remove('rotate-180');
             headings.forEach(heading => {
                 heading.classList.remove('font-semibold');
                 heading.classList.add('font-normal');
