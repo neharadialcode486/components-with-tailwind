0db87c26db2a8a4a2e212ded746b08e234058d87
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-10-16 09:39:16 +0530

    tab button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+    // ============pricing table tabs================
     const monthlyTab = document.getElementById('monthlyTab');
     const fourMonthsTab = document.getElementById('fourMonthsTab');
     const monthlyContent = document.getElementById('monthlyContent');
@@ -7,7 +8,7 @@
       monthlyTab.classList.add('bg-orange-400', 'text-white');
       monthlyTab.classList.remove('bg-white', 'text-orange-400');
       fourMonthsTab.classList.remove('bg-orange-400', 'text-white');
-      fourMonthsTab.classList.add('bg-white', 'text-orange-400');
+      fourMonthsTab.classList.add('bg-white', 'text-black');
       monthlyContent.classList.remove('hidden');
       fourMonthsContent.classList.add('hidden');
     });
@@ -16,7 +17,7 @@
       fourMonthsTab.classList.add('bg-orange-400', 'text-white');
       fourMonthsTab.classList.remove('bg-white', 'text-orange-400');
       monthlyTab.classList.remove('bg-orange-400', 'text-white');
-      monthlyTab.classList.add('bg-white', 'text-orange-400');
+      monthlyTab.classList.add('bg-white', 'text-black');
       monthlyContent.classList.add('hidden');
       fourMonthsContent.classList.remove('hidden');
     });
